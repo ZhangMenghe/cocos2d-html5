700e8b871aeb3b827544a2922d21e168eceff7d1
ZhangMenghe <zmh.leaves@gmail.com>
2015-07-07 13:34:31 +0800

    add setClearColor feature to html engine.part1,canvas rendering

---
+++ cocos2d/core/CCDirectorCanvas.js
@@ -0,0 +1,106 @@
+/****************************************************************************
+ Copyright (c) 2008-2010 Ricardo Quesada
+ Copyright (c) 2011-2012 cocos2d-x.org
+ Copyright (c) 2013-2014 Chukong Technologies Inc.
+
+ http://www.cocos2d-x.org
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ ****************************************************************************/
+
+
+if (cc._renderType === cc._RENDER_TYPE_CANVAS) {
+
+    var _p = cc.Director.prototype;
+
+    _p.setProjection = function (projection) {
+        this._projection = projection;
+        cc.eventManager.dispatchEvent(this._eventProjectionChanged);
+    };
+
+    _p.setDepthTest = function () {
+    };
+
+    _p.setClearColor = function(clearColor,alpha) {
+        var context = cc._renderContext;
+        var currentColor = clearColor || new cc.math.Vec3(0,0,0);
+        var ColorArray = clearColor.toTypeArray();        
+        var fillStyle = 'rgb(' +ColorArray[0]+','+ColorArray[1]+','+ColorArray[2]+')' ;
+        context.setFillStyle(fillStyle);
+        context.setGlobalAplha(alpha);
+        context.fillRect(context._offsetX, context._offsetY, context.canvas.height, context.canvas.width);
+    };
+    _p.setOpenGLView = function (openGLView) {
+        // set size
+        this._winSizeInPoints.width = cc._canvas.width;      //this._openGLView.getDesignResolutionSize();
+        this._winSizeInPoints.height = cc._canvas.height;
+        this._openGLView = openGLView || cc.view;
+        if (cc.eventManager)
+            cc.eventManager.setEnabled(true);
+    };
+
+    _p._clear = function () {
+        var viewport = this._openGLView.getViewPortRect();
+        var context = cc._renderContext.getContext();
+        context.setTransform(1,0,0,1, 0, 0);
+        context.clearRect(-viewport.x, viewport.y, viewport.width, viewport.height);
+    };
+
+    _p._createStatsLabel = function () {
+        var _t = this;
+        var fontSize = 0;
+        if (_t._winSizeInPoints.width > _t._winSizeInPoints.height)
+            fontSize = 0 | (_t._winSizeInPoints.height / 320 * 24);
+        else
+            fontSize = 0 | (_t._winSizeInPoints.width / 320 * 24);
+
+        _t._FPSLabel = new cc.LabelTTF("000.0", "Arial", fontSize);
+        _t._SPFLabel = new cc.LabelTTF("0.000", "Arial", fontSize);
+        _t._drawsLabel = new cc.LabelTTF("0000", "Arial", fontSize);
+
+        var locStatsPosition = cc.DIRECTOR_STATS_POSITION;
+        _t._drawsLabel.setPosition(_t._drawsLabel.width / 2 + locStatsPosition.x, _t._drawsLabel.height * 5 / 2 + locStatsPosition.y);
+        _t._SPFLabel.setPosition(_t._SPFLabel.width / 2 + locStatsPosition.x, _t._SPFLabel.height * 3 / 2 + locStatsPosition.y);
+        _t._FPSLabel.setPosition(_t._FPSLabel.width / 2 + locStatsPosition.x, _t._FPSLabel.height / 2 + locStatsPosition.y);
+    };
+
+    _p.getVisibleSize = function () {
+        //if (this._openGLView) {
+        //return this._openGLView.getVisibleSize();
+        //} else {
+        return this.getWinSize();
+        //}
+    };
+
+    _p.getVisibleOrigin = function () {
+        //if (this._openGLView) {
+        //return this._openGLView.getVisibleOrigin();
+        //} else {
+        return cc.p(0, 0);
+        //}
+    };
+} else {
+    cc.Director._fpsImage = new Image();
+    cc._addEventListener(cc.Director._fpsImage, "load", function () {
+        cc.Director._fpsImageLoaded = true;
+    });
+    if (cc._fpsImage) {
+        cc.Director._fpsImage.src = cc._fpsImage;
+    }
+}
